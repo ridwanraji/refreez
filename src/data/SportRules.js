692be4b4e61f0c5c50e38de7dea4689b708d7cd9
RIDWAN RAJI <ror716@mail.usask.ca>
2019-12-31 05:38:53 -0600

    changed the data from json to js object

---
+++ src/data/SportRules.js
@@ -0,0 +1,14 @@
+const SportRules = [
+  {
+    id: 1,
+    name: "Football (Soccer)",
+    shortDescription: "Association football, more commonly known as football or soccer, is a team sport played with a spherical ball between two teams of eleven players. It is played by 250 million players in over 200 countries and dependencies, making it the world's most popular sport. The game is played on a rectangular field called a pitch with a goal at each end. The object of the game is to score by moving the ball beyond the goal line into the opposing goal."
+  },
+  {
+    id: 2,
+    name: "American Football",
+    shortDescription: "American football, referred to as football in the United States and Canada and also known as gridiron, is a team sport played by two teams of eleven players on a rectangular field with goalposts at each end. The offense, the team with possession of the oval-shaped football, attempts to advance down the field by running with the ball or passing it, while the defense, the team without possession of the ball, aims to stop the offense's advance and to take control of the ball for themselves. The offense must advance at least ten yards in four downs or plays; if they fail, they turn over the football to the defense, but if they succeed, they are given a new set of four downs to continue the drive. Points are scored primarily by advancing the ball into the opposing team's end zone for a touchdown or kicking the ball through the opponent's goalposts for a field goal. The team with the most points at the end of a game wins."
+  }
+];
+
+export default SportRules;
